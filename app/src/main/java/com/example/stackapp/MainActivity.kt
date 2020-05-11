06d333e3ccda26e86a7d4037f4fe4bce89e64701
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2020-05-11 09:17:26 +0200

    first commit

---
+++ app/src/main/java/com/example/stackapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.stackapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.stackapp.fragments.AboutFragment
+import com.example.stackapp.fragments.QuestionsFragment
+import com.example.stackapp.fragments.SettingsFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun menuClick(view: View) {
+        when (view.id){
+            R.id.btnMenuQuestions -> QuestionsFragment().load()
+            R.id.btnMenuSettings -> SettingsFragment().load()
+            R.id.btnMenuAbout -> AboutFragment().load()
+        }
+    }
+
+    private fun Fragment.load() = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, this)
+            .commit()
+
+}
